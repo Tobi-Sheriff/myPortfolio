517224a07ca361fff40362f01e0bfacee5ce4d19
Tobi Sheriff <s.oluwatobiloba30@gmail.com>
2022-09-10 20:24:14 +0100

    portfolio first upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const ejsMate = require('ejs-mate')
+const path = require('path')
+const app = express();
+
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/tobisheriff', (req, res) => {
+    res.render('tobisheriff');
+})
+
+
+app.listen('3000', () => {
+    console.log('Serving your site on port 3000');
+})
